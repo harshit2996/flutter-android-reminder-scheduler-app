3dcb3b1c0eefe69cf818459202e1dc5b8c6fc902
Harshit Singh <harshitsingh2996@gmail.com>
2020-10-26 18:04:35 +0530

    Feat: Integrated Notifications with Reminders

--- lib/datepicker.dart
+++ lib/datepicker.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 pd(BuildContext context) {}
 
@@ -25,39 +26,132 @@ class DatePickerState extends State<DatePicker> {
     11: "November",
     12: "December"
   };
+  FlutterLocalNotificationsPlugin fltrNotification =
+      new FlutterLocalNotificationsPlugin();
   @override
   void initState() {
     super.initState();
     dt = DateTime.now();
     time = TimeOfDay.now();
+    var androidInitialize = new AndroidInitializationSettings('app_icon');
+    var iOSinitialize = new IOSInitializationSettings();
+    var initilizationsSettings =
+        new InitializationSettings(androidInitialize, iOSinitialize);
+    fltrNotification.initialize(initilizationsSettings,
+        onSelectNotification: notificationSelected);
+  }
+
+  Future _sNotification(DateTime scheduledTime, int id) async {
+    var androidDetails = new AndroidNotificationDetails(
+        "1", "Epap", "EpapNotificationChannel",
+        importance: Importance.Max);
+    var iOSDetails = new IOSNotificationDetails();
+    var generalNotificationDetails =
+        new NotificationDetails(androidDetails, iOSDetails);
+
+    // await fltrNotification.show(
+    //     0, "Task", "You created a Task", generalNotificationDetails,
+    //     payload: "Task");
+    // var scheduledTime = DateTime.now().add(Duration(seconds: 10));
+    // scheduledTime.add()
+    fltrNotification.schedule(
+        id, "Epap", "Reminder", scheduledTime, generalNotificationDetails);
+  }
+
+  Future cancelAllNotifications() async {
+    await fltrNotification.cancelAll();
+    setState(() {
+      alarms = [];
+    });
+  }
+
+  Future cancelNotification(int id) async {
+    await fltrNotification.cancel(id);
+    alarms.removeAt(id);
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-          // padding: const EdgeInsets.all(8),
-          itemCount: alarms.length,
-          itemBuilder: (BuildContext context, int index) {
-            return Container(
-                // height: 50,
-                // margin: EdgeInsets.all(2),
-                child: Column(
-                    // child: Text(
-                    children: <Widget>[
-                  ListTile(
-                    title: Text(
-                        '${alarms[index][0].day} ${monthsInYear[alarms[index][0].month]} ${alarms[index][0].year} , ${alarms[index][1].hourOfPeriod}:${getminute(alarms[index][1])} ${getm(alarms[index][1])}'),
-                    trailing: Icon(Icons.edit),
-                    onTap: () {
-                      if (alarms.isEmpty) {
-                        index = null;
-                      }
-                      editAlarm(context, index);
-                    },
-                  ),
-                ]));
-          }),
+      appBar: AppBar(title: const Text('Epap'), actions: [
+        IconButton(
+            icon: Icon(Icons.delete),
+            onPressed: () async {
+              cancelAllNotifications();
+            })
+      ]),
+      body: Column(children: [
+        Expanded(
+          child: ListView.builder(
+              // padding: const EdgeInsets.all(8),
+              itemCount: alarms.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Container(
+                  child: Column(children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 5.0),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Expanded(
+                              child: ListTile(
+                            title: Text(
+                                '${alarms[index][0].day} ${monthsInYear[alarms[index][0].month]} ${alarms[index][0].year} , ${alarms[index][1].hourOfPeriod}:${getminute(alarms[index][1])} ${getm(alarms[index][1])}'),
+                          )),
+                          IconButton(
+                            icon: Icon(Icons.edit),
+                            onPressed: () {
+                              if (alarms.isEmpty) {
+                                index = null;
+                              }
+                              editAlarm(context, index);
+                            },
+                          ),
+                          IconButton(
+                            icon: Icon(Icons.cancel),
+                            color: Colors.red,
+                            onPressed: () {
+                              cancelNotification(index);
+                            },
+                          )
+                        ],
+                      ),
+                    )
+                  ]),
+                );
+              }),
+        ),
+      ]),
+      drawer: Drawer(
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child:
+                  Text('Drawer Header', style: TextStyle(color: Colors.white)),
+              decoration: BoxDecoration(
+                color: Colors.purple[900],
+              ),
+            ),
+            ListTile(
+              title: Text('Item 1'),
+              onTap: () {
+                // Update the state of the app.
+                // ...
+              },
+            ),
+            ListTile(
+              title: Text('Item 2'),
+              onTap: () {
+                // Update the state of the app.
+                // ...
+              },
+            ),
+          ],
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           pickDate(context, null);
@@ -68,17 +162,30 @@ class DatePickerState extends State<DatePicker> {
     );
   }
 
+  Future notificationSelected(String payload) async {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        content: Text("Notification : $payload"),
+      ),
+    );
+  }
+
   picktime(BuildContext context, int index) async {
     TimeOfDay t = await showTimePicker(
       context: context,
       initialTime: (index != null) ? alarms[index][1] : TimeOfDay.now(),
     );
     if (t != null) {
-      setState(() {
-        time = t;
-      });
       int l = alarms.length;
-      alarms.insert(l, [dt, time]);
+      time = t;
+      index = l;
+      DateTime st =
+          dt.add(Duration(hours: t.hour, minutes: t.minute, seconds: 10));
+      setState(() {
+        _sNotification(st, index);
+        alarms.insert(l, [dt, time]);
+      });
     }
   }
 
@@ -129,6 +236,10 @@ class DatePickerState extends State<DatePicker> {
         if (newdateTime[0] != alarms[index][0] ||
             newdateTime[1] != alarms[index][1]) {
           alarms[index] = newdateTime;
+          DateTime nst = dt.add(Duration(
+              hours: newtime.hour, minutes: newtime.minute, seconds: 10));
+          await fltrNotification.cancel(index);
+          await _sNotification(nst, index);
           setState(() {});
         }
       }
--- lib/epap.dart
+++ lib/epap.dart
@@ -11,40 +11,8 @@ class Epap extends StatelessWidget {
           primaryColor: Colors.purple[900],
         ),
         home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Epap'),
-          ),
           body: Center(
             child: DatePicker(),
-          ),
-          drawer: Drawer(
-            child: ListView(
-              // Important: Remove any padding from the ListView.
-              padding: EdgeInsets.zero,
-              children: <Widget>[
-                DrawerHeader(
-                  child: Text('Drawer Header',
-                      style: TextStyle(color: Colors.white)),
-                  decoration: BoxDecoration(
-                    color: Colors.purple[900],
-                  ),
-                ),
-                ListTile(
-                  title: Text('Item 1'),
-                  onTap: () {
-                    // Update the state of the app.
-                    // ...
-                  },
-                ),
-                ListTile(
-                  title: Text('Item 2'),
-                  onTap: () {
-                    // Update the state of the app.
-                    // ...
-                  },
-                ),
-              ],
-            ),
           ),
         ));
   }
